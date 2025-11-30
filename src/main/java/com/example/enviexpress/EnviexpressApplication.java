ce68926f8c9a97934630666a756ae6856a6f665a
Juan <polarmagic2017@gmail.com>
2025-11-30 10:04:55 -0500

    Inicializacion Repo 30/11

---
+++ src/main/java/com/example/enviexpress/EnviexpressApplication.java
@@ -0,0 +1,13 @@
+package com.example.enviexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviexpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/enviexpress/EnviexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enviexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
